6863e249d45dc7efbe09ff0849e3f39006a3f6ab
BrunoDiasLS <brunodlopes82@globo.com>
2022-10-11 14:00:07 -0300

    Estudo Arrays

---
+++ src/main/java/Exercicio2.java
@@ -0,0 +1,11 @@
+//2) Faça um programa que exiba os números de 1 até 2000.
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 2000; i++) {
+            System.out.println(i);
+        }
+    }
+}
