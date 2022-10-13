896161cef7c13797a5c8a00f920d875bbd1880b7
BrunoDiasLS <brunodlopes82@globo.com>
2022-10-13 14:00:41 -0300

    Estudo Data, Enums, Listas e Strings

---
+++ src/main/java/Exercicio4.java
@@ -0,0 +1,14 @@
+/*
+4) Criar um programa que lê a idade de uma pessoa e diz se ela é maior de idade ou não.
+ */
+public class Exercicio4 {
+    public static void main(String[] args) {
+        int idade = 0;
+
+        if(idade >= 18){
+            System.out.println("Maior de idade.");
+        } else {
+            System.out.println("Menor de idade.");
+        }
+    }
+}
